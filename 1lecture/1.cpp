2c7a0024dccc314e0daea3b5e52677b0a77e42b1
Asylniet <zasylniet@mail.ru>
2025-10-20 19:05:59 +0500

    1lecture

--- 1lecture/1.cpp
+++ 1lecture/1.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-  cout << "Hello World!" << endl;
+  int n;
+  cin >> n;
+
+  // 3
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++) {
+      if(i == j || i == n - 1 - j) {
+        cout << 1 << " ";
+      } else {
+        cout << 0 << " ";
+      }
+      // i == j ? cout << 1 << " " : cout << 0 << " ";
+    }
+    cout << endl;
+  }
+
+  // i = 0, j = 2, n = 3
+  // i = 1, j = 1, n = 3
+  // i = 2, j = 0, n = 3
+  // i = n - 1 - j
+
+  // int i = 0;
+  // while(i < n) {
+  //   cout << i << endl;
+  //   i++;
+  // }
+
   return 0;
 }
