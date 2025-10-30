bb301bd95ea3cd741c31fa5a5ee34294f28c4e0c
Asylniet <zasylniet@mail.ru>
2025-10-30 21:14:48 +0500

    feat: 3lecture

--- 3lecture/3.cpp
+++ 3lecture/3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -16,12 +17,43 @@ using namespace std;
 // a - a ->
 // s - g -X ---- "adga" || "asda" -- NO
 
+bool isPalindrome(string s) {
+  for(int i = 0; i < s.size() / 2; i++) {
+    int end_index = s.size() - 1 - i;
+    if(s[i] != s[end_index]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+string removeChar(string s, int index) {
+  string newStr = "";
+  for(int i = 0; i < s.size(); i++) {
+    if(i != index) newStr += s[i];
+  }
+  return newStr;
+}
+
 
 int main() {
   string s;
   cin >> s;
-
   
+  for(int i = 0; i < s.size() / 2; i++) {
+    int end_index = s.size() - 1 - i;
+    if(s[i] != s[end_index]) {
+      string firstStr = removeChar(s, i);
+      string secondStr = removeChar(s, end_index);
+      if(!isPalindrome(firstStr) && !isPalindrome(secondStr)) {
+        cout << "NO";
+        return 0;
+      }
+    }
+  }
+
+  cout << "YES";
 
   return 0;
 }
